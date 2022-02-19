6bd9ee42dff3bf5ad557925f6391937457274ee5
Shawn Cicoria <github@cicoria.com>
2022-02-19 11:40:21 -0500

    missing 2 things
    shifting the alpha string by ROT
    
    converting the string ROT to INT

--- project1/v1/caesar.s
+++ project1/v1/caesar.s
@@ -13,7 +13,7 @@
 
 
 .bss
-    .comm   input  10                   # plaintext message 10 now for testing only will be 51
+    .comm   plaintext  51                   # plaintext message 10 now for testing only will be 51
     .comm   rotinput 5                  # number up to 1000 plus EOL
 
 
@@ -27,14 +27,41 @@
 
   Encrypt:
     nop
-    ret
+    # prolog
+    pushl %ebp              # store the current value of EBP on the stack
+    movl %esp, %ebp         # Make EBP point to top of stack
+
+    # stack
+    movl 8(%ebp), %ecx      # pointer to plaintext
+    movl 12(%ebp), %edx     # rot length
+
+      # debug
+      pushl $51
+      pushl 8(%ebp)
+      call PrintFunction
+      # debug
+      pushl $5
+      pushl 12(%ebp)
+      call PrintFunction
+      # debug - end
+
+    # logic
+
+
+    # epilog
+    movl %ebp, %esp         # Restore the old value of ESP
+    popl %ebp               # Restore the old value of EBP
+    ret   
+
+
+    
 
   GetInput1:
     nop
     movl $3, %eax
     movl $0, %ebx
-    movl $input, %ecx
-    movl $10, %edx
+    movl $plaintext, %ecx
+    movl $51, %edx
     int $0x80
     ret
     
@@ -42,8 +69,8 @@
     nop
     movl $3, %eax
     movl $0, %ebx
-    movl $input, %ecx
-    movl $10, %edx
+    movl $rotinput, %ecx
+    movl $5, %edx
     int $0x80
     ret
 
@@ -52,10 +79,10 @@
     movl %esp, %ebp         # Make EBP point to top of stack
 
     # The write function
-    movl $4, %eax
-    movl $1, %ebx
-    movl 8(%ebp), %ecx
-    movl 12(%ebp), %edx
+    movl $4, %eax           # write call
+    movl $1, %ebx           # stdout
+    movl 8(%ebp), %ecx      # message pointer
+    movl 12(%ebp), %edx     # message length
     int $0x80
 
     movl %ebp, %esp         # Restore the old value of ESP
@@ -89,17 +116,49 @@ _start:
   
   call GetInput2
 
+
+
   # at least this should use a stack...
+  # push length
+  pushl $rotinput
+  # push pointer
+  pushl $plaintext
   call Encrypt
 
 
-  # push the strlen on the stack
-  pushl $prompt3len
-  # push the string pointer on the stack
-  pushl $prompt3
-  call PrintFunction
 
-  ; call _exit
+  // # push the strlen on the stack
+  // pushl $prompt3len
+  // # push the string pointer on the stack
+  // pushl $prompt3
+  // call PrintFunction
+
+  // # push the strlen on the stack
+  // pushl $51
+  // # push the string pointer on the stack
+  // pushl $plaintext
+  // call PrintFunction
+
+
+
+  // # TODO: remove 
+  // # DEBUG
+  // # push the strlen on the stack
+  // pushl $51
+  // # push the string pointer on the stack
+  // pushl $plaintext
+  // call PrintFunction
+  
+
+  // # push the strlen on the stack
+  // pushl $5
+  // # push the string pointer on the stack
+  // pushl $rotinput
+  // call PrintFunction  
+
+  # debug end
+
+  call _exit
 
   _exit:
 
